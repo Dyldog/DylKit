9bc4f8d8645b1c1e5e5a4708319c7a1bab230a9a
Dylan Elliott <dylan.elliott@gmail.com>
2025-06-23 19:15:07 +1000

    Add result view

---
+++ Sources/DylKit/UI/Components/SwiftUI/ResultView.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dylan Elliott on 23/6/2025.
+//
+
+import SwiftUI
+
+public struct ResultView<Success, Failure: Error, SuccessView: View, FailView: View>: View {
+    let result: Result<Success, Failure>?
+    let success: (Success) -> SuccessView
+    let failure: (Failure) -> FailView
+    
+    public init(
+        result: Result<Success, Failure>?,
+        success: @escaping (Success) -> SuccessView,
+        failure: @escaping (Failure) -> FailView
+    ) {
+        self.result = result
+        self.success = success
+        self.failure = failure
+    }
+    
+    public var body: some View {
+        switch result {
+        case .none:
+            ProgressView().progressViewStyle(.circular)
+        case .success(let data):
+            success(data)
+        case .failure(let error):
+            failure(error)
+        }
+    }
+}
+
+public extension ResultView where Success: RandomAccessCollection {
+    init<SuccessRow: View>(
+        _ result: Result<Success, Failure>?,
+        success: @escaping (Success.Element) -> SuccessRow,
+        failure: @escaping (Failure) -> FailView
+    ) where SuccessView == ForEach<[(offset: Int, element: Success.Element)], Int, SuccessRow> {
+        self.init(
+            result: result, 
+            success: { data in
+                ForEach(data) {
+                    success($0)
+                }
+            }, 
+            failure: failure
+        )
+    }
+}
