da22d16159ee1de9d3238f2452d2c0b6ed8343db
Dylan Elliott <dylan.elliott@gmail.com>
2025-06-27 17:28:13 +1000

    Add SwiftSoup dependency

--- Package.swift
+++ Package.swift
@@ -18,14 +18,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Dyldog/HighlightedTextEditor", branch: "main")
+        .package(url: "https://github.com/Dyldog/HighlightedTextEditor", branch: "main"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.8.8")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DylKit",
-            dependencies: ["HighlightedTextEditor"]),
+            dependencies: ["HighlightedTextEditor", "SwiftSoup"]),
         .testTarget(
             name: "DylKitTests",
             dependencies: ["DylKit"]),
