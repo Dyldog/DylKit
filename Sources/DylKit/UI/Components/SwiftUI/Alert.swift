82d87b0fc716164b94c42f0c6629ac3440bad1aa
Dylan Elliott <dylan.elliott@gmail.com>
2025-06-27 17:27:55 +1000

    Add buttons to Alert

--- Sources/DylKit/UI/Components/SwiftUI/Alert.swift
+++ Sources/DylKit/UI/Components/SwiftUI/Alert.swift
@@ -11,17 +11,39 @@ public struct Alert: Identifiable {
     public let id: UUID = .init()
     let title: String
     let message: String
+    let primaryAction: (String, () -> Void)
+    let secondaryAction: (String, () -> Void)?
     
-    public init(title: String, message: String) {
+    public init(
+        title: String,
+        message: String,
+        primaryAction: (String, () -> Void) = ("OK", {}),
+        secondaryAction: (String, () -> Void)? = nil
+    ) {
         self.title = title
         self.message = message
+        self.primaryAction = primaryAction
+        self.secondaryAction = secondaryAction
     }
 }
 
 public extension View {
     func alert(_ binding: Binding<Alert?>) -> some View {
-        self.alert(item: binding) {
-            SwiftUI.Alert(title: Text($0.title), message: Text($0.message))
+        self.alert(item: binding) { alert in
+            if let secondaryAction = alert.secondaryAction {
+                SwiftUI.Alert(
+                    title: Text(alert.title),
+                    message: Text(alert.message),
+                    primaryButton: .default(Text(alert.primaryAction.0), action: alert.primaryAction.1),
+                    secondaryButton: .default(Text(secondaryAction.0), action: secondaryAction.1)
+                )
+            } else {
+                SwiftUI.Alert(
+                    title: Text(alert.title),
+                    message: Text(alert.message),
+                    dismissButton: .default(Text(alert.primaryAction.0), action: alert.primaryAction.1)
+                )
+            }
         }
     }
 }
